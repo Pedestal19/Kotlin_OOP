97b14c8e673b33072f7332e3238c77d1d8bf09b1
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-08 23:06:41 +0100

    first commit

---
+++ src/SimpleClass.kt
@@ -0,0 +1,20 @@
+/**
+ * Author: Hosanna Gabe-Oji.
+ *Project: Kotlin_OOP.
+ *Date: 3/8/2020.
+ */
+
+class Car() {
+
+    init {
+        println("class is created")
+    }
+
+}
+
+fun main(args:Array<String>){
+
+    val hosannaCar = Car()
+    val moninsehCar = Car()
+
+}
