881909b2d3eef907ebcd1549132fc010bbda81e2
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-09 23:15:23 +0100

    casting

--- src/Overriding.kt
+++ src/Overriding.kt
@@ -17,6 +17,10 @@ open class Operations1(){
 
 class MultiOperations1():Operations1(){
 
+    /*constructor(): super(){
+
+    }*/
+
     override fun sum(n1:Int, n2:Int):Int{
         return n1+n2*3
     }
@@ -44,15 +48,15 @@ fun main(args:Array<String>){
 
     println("op.ProcessName "+op.ProcessName)
 
-    var op2=MultiOperations1()
+    var op2=MultiOperations1() as Operations1
     sum=op2.sum(10, 15)
     println("sum: "+ sum)
     div = op2.div(12,11)
     println("div: "+div)
-    var mult=op2.mult(10, 15)
-    println("multi: "+ mult)
-    var sub = op2.sub(12,11)
-    println("div: "+sub)
+//    var mult=op2.mult(10, 15)
+//    println("multi: "+ mult)
+//    var sub = op2.sub(12,11)
+//    println("div: "+sub)
 
     //public can be accessed through inherited class and can be read through an instance
     //protected is same as public but cannot be read through an instance
