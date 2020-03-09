9b4a3e11aff7b0c412c9335fb26f17ef63c7447c
HosannaGabeOji <gabeoji01@gmail.com>
2020-03-09 22:07:01 +0100

    visible modifiers

--- src/ClassInheritance.kt
+++ src/ClassInheritance.kt
@@ -5,7 +5,7 @@
  */
 
 open class Operations(){
-
+    var ProcessName:String?=null
     fun sum(n1:Int, n2:Int):Int{
         return n1+n2
     }
@@ -26,6 +26,10 @@ class MultiOperations():Operations(){
     fun mult(n1:Int, n2:Int):Int{
         return n1*n2
     }
+
+    fun GetName(){
+        super.ProcessName
+    }
 }
 
 fun main(args:Array<String>){
@@ -35,6 +39,8 @@ fun main(args:Array<String>){
     println("sum: "+ sum)
     var div = op.div(12,11)
     println("div: "+div)
+
+    println("op.ProcessName "+op.ProcessName)
 
     var op2=MultiOperations()
     sum=op2.sum(10, 15)
@@ -46,4 +52,7 @@ fun main(args:Array<String>){
     var sub = op2.sub(12,11)
     println("div: "+sub)
 
+    //public can be accessed through inherited class and can be read through an instance
+    //protected is same as public but cannot be read through an instance
+    //private is only visible to the class
 }
